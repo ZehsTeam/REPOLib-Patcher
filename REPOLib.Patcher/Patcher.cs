8c7d54ee9c9392d3017d155f65ef75d37660b36d
Zehs <andrey5764zehsteam@gmail.com>
2025-11-04 00:53:56 -0600

    Add project files.

---
+++ REPOLib.Patcher/Patcher.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using Mono.Cecil;
+using System.Collections.Generic;
+using System.IO;
+
+namespace com.github.zehsteam.REPOLib_Patcher;
+
+internal static class Patcher
+{
+    public static IEnumerable<string> TargetDLLs { get; } = [];
+
+    private static IEnumerable<string> GetAllPluginDLLs()
+    {
+        return Directory.GetFiles(Paths.PluginPath, "*.dll", SearchOption.AllDirectories);
+    }
+
+    public static void Initialize()
+    {
+
+    }
+
+    private static void CheckPlugin(string filePath)
+    {
+        
+    }
+
+    public static void Patch(AssemblyDefinition _)
+    {
+
+    }
+}
